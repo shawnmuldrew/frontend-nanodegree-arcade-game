62278625f080ecdccda16e26d37fad46d6f5510c
Shawn Muldrew <shawnmuldrew@gmail.com>
2015-12-09 16:29:25 -0700

    add movement and collisions

--- js/app.js
+++ js/app.js
@@ -14,13 +14,30 @@ var Enemy = function(speed,x,y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-  if (this.x > 500) {
-    this.x = -100;
-  } 
+  // Move enemy
   this.x = this.x + this.speed*dt; 
+  
+  //Check for collisions with player
+  var enemyFront = this.x+50;
+  var enemyBack = this.x-50;
+  var playerFront = player.x-30;
+  var playerBack = player.x+30;
+  if (this.y == player.y) {
+    if (((enemyFront > playerFront) && (enemyFront < playerBack)) || ((enemyBack > playerFront) && (enemyBack < playerBack))) {
+      player.x = playerStartX;
+      player.y = playerStartY;
+      points = 0;
+    }
+  }
+
+  // If off screen remove from array and start new enemy
+  if (this.x > 500) {
+    var doneEnemy = allEnemies.indexOf(this);
+    allEnemies.splice(doneEnemy,1);
+    var yIndex = Math.floor(Math.random() * 3);  
+    var enemySpeed = Math.floor(Math.random() * (enemySpeedMax-enemySpeedMin)) + enemySpeedMin; 
+    allEnemies.push(new Enemy(enemySpeed,enemyStartX,enemyStartY[yIndex]));
+  } 
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,17 +45,11 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
 var Player = function(x,y) {
   this.sprite = 'images/char-boy.png';
   this.x = x;
   this.y = y;
-};
-
-Player.prototype.update = function(dt) {
-  //SM - add movement
 };
 
 Player.prototype.render = function() {
@@ -48,31 +59,59 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(key) {
   console.log(key);
   if (key == 'up') {
-    this.y = this.y - 82;
+    if ((this.y - playerMoveY) >= playerUpFinish) {
+      this.y = this.y - playerMoveY;
+      points++;
+    }
+    else {
+      this.y = playerStartY;
+    }
   }
   if (key == 'down') {
-    this.y = this.y + 82;
+    if ((this.y + playerMoveY) <= playerDownEdge) {
+      this.y = this.y + playerMoveY;
+      points--;
+    }
   }
   if (key == 'left') {
-    this.x = this.x - 100;
+    if ((this.x - playerMoveX) >= playerLeftEdge) {
+      this.x = this.x - playerMoveX;
+    }
   }
   if (key == 'right') {
-    this.x = this.x + 100;
+    if ((this.x + playerMoveX) <= playerRightEdge) {
+      this.x = this.x + playerMoveX;
+    }
   }
+  console.log('points = '.concat(points));
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player1 = new Player(1,400);
-var enemy1 = new Enemy(100,-100,225);
-var enemy2 = new Enemy(200,-100,145);
-var enemy3 = new Enemy(50,-100,60);
-
+var numEnemies = 4;
 var allEnemies = [];
-allEnemies.push(enemy1);
-allEnemies.push(enemy2);
-allEnemies.push(enemy3);
+var points = 0;
+var playerStartX = 201;
+var playerStartY = 390;
+var playerMoveX = 100;
+var playerMoveY = 82;
+var playerLeftEdge = 1;
+var playerDownEdge = 462;
+var playerRightEdge = 401;
+var playerUpFinish = 62;
+var enemyStartX = -100;
+var enemyStartY = [62,144,226];
+var enemySpeedMax = 300;
+var enemySpeedMin = 100;
+
+var player1 = new Player(playerStartX,playerStartY);
+
+for (i=0; i<numEnemies; i++) {
+  var yIndex = Math.floor(Math.random() * 3);  
+  var enemySpeed = Math.floor(Math.random() * (enemySpeedMax-enemySpeedMin)) + enemySpeedMin; 
+  allEnemies.push(new Enemy(enemySpeed,enemyStartX,enemyStartY[yIndex]));
+};
 
 var player = player1;
 
