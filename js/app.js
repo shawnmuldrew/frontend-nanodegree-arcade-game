46f2fc35c15deb138ae10e5087c2fc35ade67ee2
Shawn Muldrew <shawnmuldrew@gmail.com>
2015-12-10 22:55:01 -0700

    Add gems and player reset method

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 // Enemies our player must avoid
 var Enemy = function(speed,x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
   this.x = x;
   this.y = y;
@@ -52,19 +47,31 @@ var Player = function(x,y) {
   this.y = y;
 };
 
+// Player update method - not needed
+Player.prototype.update = function(dt) {
+};
+
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Player.prototype.reset = function() {
+ this.y = playerStartY;
+ this.x = playerStartX;
+};
+
+// Move player based on keyboard input
+// Ensure player does not move off board
+// Add points as player moves forward
 Player.prototype.handleInput = function(key) {
-  console.log(key);
   if (key == 'up') {
     if ((this.y - playerMoveY) >= playerUpFinish) {
       this.y = this.y - playerMoveY;
       points++;
     }
     else {
-      this.y = playerStartY;
+      player.reset();
+      points++;
     }
   }
   if (key == 'down') {
@@ -84,13 +91,26 @@ Player.prototype.handleInput = function(key) {
     }
   }
   console.log('points = '.concat(points));
+  console.log('Player Y: '.concat(this.y));
+};
+
+var Gem = function(x,y) {
+  this.sprite = 'images/gem-orange-small.png';
+  this.x = x;
+  this.y = y;
+};
+
+Gem.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var numEnemies = 4;
+var numGems = 2;
 var allEnemies = [];
+var allGems = [];
 var points = 0;
 var playerStartX = 201;
 var playerStartY = 390;
@@ -102,19 +122,33 @@ var playerRightEdge = 401;
 var playerUpFinish = 62;
 var enemyStartX = -100;
 var enemyStartY = [62,144,226];
+var GemY = [62,144,226];
+var GemX = [1,101,201,301,401,501];
 var enemySpeedMax = 300;
 var enemySpeedMin = 100;
 
+// Create player
 var player1 = new Player(playerStartX,playerStartY);
 
+//Create gems
+for (i=0; i<numGems; i++) {
+  var xIndex = Math.floor(Math.random() * 5);
+  var yIndex = Math.floor(Math.random() * 3);
+  allGems.push(new Gem(GemX[xIndex],GemY[yIndex]));
+};
+console.log("Gem X: ".concat(xIndex));
+console.log("Gem Y: ".concat(yIndex));
+console.log("Coordinate Y: ".concat(GemY[yIndex]));
+
+// Create enemies
 for (i=0; i<numEnemies; i++) {
   var yIndex = Math.floor(Math.random() * 3);  
   var enemySpeed = Math.floor(Math.random() * (enemySpeedMax-enemySpeedMin)) + enemySpeedMin; 
   allEnemies.push(new Enemy(enemySpeed,enemyStartX,enemyStartY[yIndex]));
 };
 
+// Set player variable to created player
 var player = player1;
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -145,13 +145,18 @@ var Engine = (function(global) {
      * on your enemy and player entities within app.js
      */
     function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
-         */
+        // Render gems
+        allGems.forEach(function(gem) {
+          gem.render();
+        });
+
+        // Loop through all of the objects within the allEnemies array and call
+        // the render function you have defined.
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 
+        // Render player
         player.render();
     }
 
@@ -172,7 +177,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/gem-orange-small.png'
     ]);
     Resources.onReady(init);
 
